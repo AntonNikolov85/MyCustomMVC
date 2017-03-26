972843e73761cf56790edf1eca27bdbc2651e4df
Anton Nikolov <antonio85pd@gmail.com>
2017-03-26 23:25:11 +0300

    add controller router class
    should handle http request - parse input from request, use reflection to
    create the needed controller and invoke the wanted action method from
    him, finally return http response

---
+++ CustomMVC/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomMVC.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToUpperFirst(this string str)
+        {
+            return char.ToUpper(str[0]) + str.Substring(1);
+        }
+    }
+}
