b74f87b97d719b1e80d5274650c5ecda772c63d4
Anton Nikolov <antonio85pd@gmail.com>
2017-03-29 01:03:27 +0300

    add MvcEngine and separate web app
    MvcEngine will seet up the MvcContext and run the Http Server; create
    the architecture for the web app by adding some folders, add AppStart
    class to start the project and RouteTable class to map the needed routes

---
+++ App.Web/AppStart.cs
@@ -0,0 +1,14 @@
+﻿namespace App.Web
+{
+    using CustomMVC;
+    using CustomHttpServer;
+
+    public class AppStart
+    {
+        public static void Main()
+        {
+            HttpServer server = new HttpServer(8081, RouteTable.Routes);
+            MvcEngine.Run(server, "App.Web");
+        }
+    }
+}
---
+++ App.Web/RouteTable.cs
@@ -0,0 +1,33 @@
+﻿namespace App.Web
+{
+    using CustomMVC.Routers;
+    using CustomHttpServer.Enums;
+    using CustomHttpServer.Models;
+
+    public static class RouteTable
+    {
+        public static Route[] Routes
+        {
+            get
+            {
+                return new Route[]
+                {
+                     new Route()
+                    {
+                        Name = "Controller/Action/GET",
+                        Method = RequestMethod.GET,
+                        UrlRegex = @"^/(.+)/(.+)",
+                        Callable = new ControllerRouter().Handle
+                    },
+                    new Route()
+                    {
+                        Name = "Controller/Action/POST",
+                        Method = RequestMethod.POST,
+                        UrlRegex = @"^/(.+)/(.+)",
+                        Callable = new ControllerRouter().Handle
+                    }
+                };
+            }
+        }
+    }
+}
